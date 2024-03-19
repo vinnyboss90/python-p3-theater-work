b728e6a16f30e4311e9d59a9ef41ee6e7d4070dc
vinnyegon90 <vinnyegon90@gmail.com>
2024-03-19 14:48:00 +0300

    Completed Assignment

--- lib/models.py
+++ lib/models.py
@@ -1,10 +1,58 @@
-from sqlalchemy import ForeignKey, Column, Integer, String, MetaData
-from sqlalchemy.orm import relationship, backref
+# models.py
+
+from sqlalchemy import create_engine, ForeignKey, Column, Integer, String, Boolean
+from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-convention = {
-    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
-}
-metadata = MetaData(naming_convention=convention)
+Base = declarative_base()
 
-Base = declarative_base(metadata=metadata)
+class Role(Base):
+    __tablename__ = 'roles'
+
+    id = Column(Integer, primary_key=True)
+    character_name = Column(String)
+    
+    auditions = relationship('Audition', back_populates='role')
+    
+    def actors(self):
+        return [audition.actor for audition in self.auditions]
+    
+    def locations(self):
+        return [audition.location for audition in self.auditions]
+    
+    def lead(self):
+        hired_auditions = [audition for audition in self.auditions if audition.hired]
+        if hired_auditions:
+            return hired_auditions[0]
+        else:
+            return 'no actor has been hired for this role'
+
+    def understudy(self):
+        hired_auditions = [audition for audition in self.auditions if audition.hired]
+        if len(hired_auditions) > 1:
+            return hired_auditions[1]
+        else:
+            return 'no actor has been hired for understudy for this role'
+
+
+class Audition(Base):
+    __tablename__ = 'auditions'
+
+    id = Column(Integer, primary_key=True)
+    actor = Column(String)
+    location = Column(String)
+    phone = Column(Integer)
+    hired = Column(Boolean, default=False)
+    
+    role_id = Column(Integer, ForeignKey('roles.id'))
+    role = relationship('Role', back_populates='auditions')
+    
+    def call_back(self):
+        self.hired = True
+
+
+# Create an SQLite in-memory database
+engine = create_engine('sqlite:///:memory:')
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
+session = Session()
